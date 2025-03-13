7396ac5ce4c72d86f6b0123c0d22ce9677e8bbff
choozn <65042105+choozn@users.noreply.github.com>
2025-03-13 16:12:08 +0100

    add: ly display manager

---
+++ ly/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (C) 2025 choozn
+# Installation script for ly-dm
+
+echo "[!] Started installation of the ly-Display-Manager."
+
+# Request sudo privileges
+sudo -v || { echo "[!] Failed to gain sudo access."; exit 1; }
+echo ""
+
+# Install ly
+sudo pacman --noconfirm --needed -S ly || { echo "[!] Failed to install ly. Exiting."; exit 1; }
+echo ""
+
+# Remove old ly config
+if [ -f "/etc/ly/config.ini" ]; then
+  sudo rm "/etc/ly/config.ini" || { echo "[!] Failed to remove old ly config."; exit 1; }
+fi
+
+# Link ly config
+sudo ln -s "$HOME/.config/hypr/ly/config.ini" "/etc/ly/config.ini" || { echo "[!] Failed to link ly config. Exiting."; exit 1; }
+
+# Activate ly servie
+sudo systemctl enable ly.service
+sudo systemctl disable getty@tty2.service
+
+echo -e "[!] Installation of ly-Display-Manager successful!"
