9211b039bbf3e7a817a9d684e24677c12068161e
choozn <65042105+choozn@users.noreply.github.com>
2025-05-12 22:02:14 +0200

    add: virt-manager and qemu install script

---
+++ qemu/setup.sh
@@ -0,0 +1,5 @@
+# Copyright (C) 2025 choozn
+# Installation script for virt-manager and qemu
+
+sudo pacman --noconfirm --needed -S libvirt virt-manager qemu-full || { echo "[!] Failed to install virtualization software (1). Exiting."; exit 1; }
+sudo systemctl start libvirtd
