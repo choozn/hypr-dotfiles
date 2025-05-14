e3a95159aca113983b4ec8761f65a6de7992c527
choozn <65042105+choozn@users.noreply.github.com>
2025-05-14 18:37:07 +0200

    fix: autostart libvirtd

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -2,4 +2,5 @@
 # Installation script for virt-manager and qemu
 
 sudo pacman --noconfirm --needed -S libvirt virt-manager qemu-full || { echo "[!] Failed to install virtualization software (1). Exiting."; exit 1; }
+sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
