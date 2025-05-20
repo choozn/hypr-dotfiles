427ed4c93e2a4886ef5755b565d0dc0483507466
choozn <65042105+choozn@users.noreply.github.com>
2025-05-20 11:14:18 +0200

    add: virtualization setup script

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -1,6 +1,10 @@
 # Copyright (C) 2025 choozn
 # Installation script for virt-manager and qemu
 
-sudo pacman --noconfirm --needed -S libvirt virt-manager qemu-full || { echo "[!] Failed to install virtualization software (1). Exiting."; exit 1; }
+sudo pacman --noconfirm --needed -S libvirt dnsmasq virt-manager qemu-full || { echo "[!] Failed to install virtualization software (1). Exiting."; exit 1; }
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
+sudo usermod -G libvirt -a $(whoami)
+
+# Only force connect when needed
+# virt-manager --connect qemu:///system
