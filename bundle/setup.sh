6f6f91423129cc7c4e8494fdea9badbf1689d9e4
choozn <65042105+choozn@users.noreply.github.com>
2025-06-03 20:45:44 +0200

    add: ungoogled-chromium to bundle

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -10,7 +10,7 @@ echo ""
 
 # Install optional other software
 sudo pacman --noconfirm --needed -S filelight gimp libreoffice-still obsidian syncthing vlc thunderbird cheese zathura zathura-pdf-mupdf || { echo "[!] Failed to install optional software (1). Exiting."; exit 1; }
-yay --noconfirm --needed -S zen-browser-bin proton-vpn-gtk-app localsend-bin tex-match || { echo "[!] Failed to install optional software (2). Exiting."; exit 1; }
+yay --noconfirm --needed -S zen-browser-bin ungoogled-chromium-bin proton-vpn-gtk-app localsend-bin tex-match || { echo "[!] Failed to install optional software (2). Exiting."; exit 1; }
 
 systemctl --user enable syncthing
 systemctl --user start syncthing
